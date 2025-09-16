bd579e528a183f17743ffd0ac9c265110b0a87db
Lucas <animesbr014@gmail.com>
2025-09-16 16:37:18 -0300

    pequena melhoria no index base

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
     </div>
 
  
- 
-        <img src="./Imagem/Logo Team Mochila.png" class="img-fluid" alt="Logo na Mochila Errada" max-width: 100%  max-height: 100% />
-    
-    
+    <div class="row">
+    <div class="col-6">
+      <img src="./Imagem/Logo Team Mochila.png" class="img-fluid" alt="Logo na Mochila Errada" max-width: 100%  max-height: 100% />
+    </div>   
+    <div class="col-6">
+      <h5 class=" fs-1">Mensagem motivadora a respeito do site</h5>
+      </div>
+    </div>
     
     <?php   include "rodape.php";?>
